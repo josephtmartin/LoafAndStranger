a9432bb5381c34b13334636259943b8fc9a11030
joe <goldglovejo13@gmail.com>
2021-02-20 10:28:34 -0600

    Add project files.

---
+++ LoafAndStranger/Models/Loaf.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoafAndStranger.Models
+{
+    //Models are for storing pieces of information, not for having behavior
+    public class Loaf
+    {
+        public LoafSize Size { get; set; }
+        public string Type { get; set; }
+        public decimal Price { get; set; }
+        public int WeightInOunces { get; set; }
+        public bool Sliced { get; set; }
+    }
+    public enum LoafSize
+    {
+        Small,
+        Medium,
+        Large
+    }
+}
