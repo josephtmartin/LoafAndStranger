659a64f01c062981156b30cd9003a8ad0967e24b
joe <goldglovejo13@gmail.com>
2021-03-16 20:48:30 -0500

    added a tops repo, model, and controller

---
+++ LoafAndStranger/DataAccess/TopsRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LoafAndStranger.Models;
+using Microsoft.Data.SqlClient;
+using Dapper;
+
+namespace LoafAndStranger.DataAccess
+{
+    public class TopsRepository
+    {
+        const string ConnectionString = "Server=localhost;Database=LoafAndStranger;Trusted_Connection=True;";
+
+        public IEnumerable<Top> GetAll()
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = @"SELECT *
+                        FROM Tops";
+
+            var tops = db.Query<Top>(sql);
+
+            return tops;
+        }
+    }
+}
